db2fd9d1391a613bb7d3251a0d04117fd74c6f12
CaDiBob <nikolay.v.byankin@gmail.com>
2021-12-03 22:23:19 +0900

    publish photo to Telegram channel

--- bot.py
+++ bot.py
@@ -6,4 +6,4 @@ load_dotenv()
 token = os.getenv('TG_API')
 chat_id = '@CaD_space_images'
 bot = telegram.Bot(token=token)
-bot.send_message(chat_id=chat_id, text="Hi my friends it`s my first message!")
+bot.send_document(chat_id=chat_id, document=open('nasa_epic/nasa_epic0.png', 'rb'))
