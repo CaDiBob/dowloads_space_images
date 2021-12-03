50896d13f6a3945161a97236cf093e57babe5f73
CaDiBob <nikolay.v.byankin@gmail.com>
2021-12-03 22:38:10 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,6 +4,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 token = os.getenv('TG_API')
-chat_id = '@CaD_space_images'
+chat_id = os.getenv('TG_CHAT_ID')
 bot = telegram.Bot(token=token)
 bot.send_document(chat_id=chat_id, document=open('nasa_epic/nasa_epic0.png', 'rb'))
